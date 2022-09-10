67ccbbea4a38bb04bec21aebc26bd23930bc3ceb
Rohit Bahuguna <rohitbahuguna.4446@gmail.com>
2022-09-10 19:10:52 +0530

    docs updated

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,7 @@ const BigPronise = require('../middlewares/bigPromise');
 exports.home = BigPronise((req, res) => {
 	res.status(200).json({
 		success: true,
-		greeting: 'Hello from API'
+		greeting: 'Hello from Rohit Bahuguna'
 	});
 });
 
